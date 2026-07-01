c776d18624e56a4018d2f95dcddf489fd7b77dab
HarshitaGhildiyal <harshitaghildiyal2501@gmail.com>
2026-07-02 01:07:19 +0530

    Command Pattern

---
+++ DesignPatterns/CommandPattern/CommandTest.java
@@ -0,0 +1,89 @@
+package DesignPatterns.CommandPattern;
+
+
+interface Command {
+    void execute();
+}
+
+// Receiver Class
+class Light {
+
+    public void turnOn() {
+        System.out.println("Light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Light is OFF");
+    }
+}
+
+// Concrete Command - Light ON
+class LightOnCommand implements Command {
+
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+// Concrete Command - Light OFF
+class LightOffCommand implements Command {
+
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+// Invoker Class
+class RemoteControl {
+
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
+// Test Class
+public class CommandTest {
+
+    public static void main(String[] args) {
+
+        // Receiver
+        Light light = new Light();
+
+        // Commands
+        Command lightOn = new LightOnCommand(light);
+        Command lightOff = new LightOffCommand(light);
+
+        // Invoker
+        RemoteControl remote = new RemoteControl();
+
+        // Turn ON
+        remote.setCommand(lightOn);
+        System.out.println("Pressing ON Button:");
+        remote.pressButton();
+
+        // Turn OFF
+        remote.setCommand(lightOff);
+        System.out.println("\nPressing OFF Button:");
+        remote.pressButton();
+    }
+}
